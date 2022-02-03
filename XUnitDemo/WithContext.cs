6617ffe851dfa75c482b13634ec6d96edf74e481
Howard Choi <hchoi84@gmail.com>
2022-02-03 11:55:06 -0800

    Section14-133

--- XUnitDemo/WithContext.cs
+++ XUnitDemo/WithContext.cs
@@ -5,6 +5,9 @@ using Xunit.Abstractions;
 
 namespace XUnitDemo
 {
+  //this attribute along with any given name is used throughout classes where we want to run it one by one. Otherwise, all the classes will run in parallel by default with the use of IClassFixture
+  //[Collection("Sequence")]
+  //to run the test in cli, use "dotnet test" from the project folder directory
   public class WithContext : IClassFixture<WebDriverFixture>
   {
     // for printing to the Test Detail Summary
@@ -37,38 +40,38 @@ namespace XUnitDemo
       driver.FindElement(By.LinkText("Login")).Click();
       driver.FindElement(By.Id("UserName")).SendKeys(username);
       driver.FindElement(By.Id("Password")).SendKeys(password);
-      driver.FindElement(By.CssSelector(".btn-default")).Click();
+      //driver.FindElement(By.CssSelector(".btn-default")).Click();
 
       _testOutputHelper.WriteLine("Test Completed");
     }
 
-    [Theory]
-    [MemberData(nameof(Data))]
-    //https://andrewlock.net/creating-parameterised-tests-in-xunit-with-inlinedata-classdata-and-memberdata/
-    public void ClassFixtureTestMemberData(string un, string pwd, string cpwd, string email)
-    {
-      var driver = _webDriverFixture.ChromeDriver;
-      driver.Navigate().GoToUrl("http://eaapp.somee.com");
+    //[Theory]
+    //[MemberData(nameof(Data))]
+    ////https://andrewlock.net/creating-parameterised-tests-in-xunit-with-inlinedata-classdata-and-memberdata/
+    //public void ClassFixtureTestMemberData(string un, string pwd, string cpwd, string email)
+    //{
+    //  var driver = _webDriverFixture.ChromeDriver;
+    //  driver.Navigate().GoToUrl("http://eaapp.somee.com");
 
-      driver.FindElement(By.LinkText("Register")).Click();
-      driver.FindElement(By.Id("UserName")).SendKeys(un);
-      driver.FindElement(By.Id("Password")).SendKeys(pwd);
-      driver.FindElement(By.Id("ConfirmPassword")).SendKeys(cpwd);
-      driver.FindElement(By.Id("Email")).SendKeys(email);
+    //  driver.FindElement(By.LinkText("Register")).Click();
+    //  driver.FindElement(By.Id("UserName")).SendKeys(un);
+    //  driver.FindElement(By.Id("Password")).SendKeys(pwd);
+    //  driver.FindElement(By.Id("ConfirmPassword")).SendKeys(cpwd);
+    //  driver.FindElement(By.Id("Email")).SendKeys(email);
 
-      _testOutputHelper.WriteLine("Test Completed");
-    }
+    //  _testOutputHelper.WriteLine("Test Completed");
+    //}
 
-    public static IEnumerable<object[]> Data => new List<object[]>
-    {
-      new object[]
-      {
-        "UserName1", "Password1", "Password1", "abc1@email.com"
-      },
-      new object[]
-      {
-        "UserName2", "Password2", "Password2", "abc2@email.com"
-      }
-    };
+    //public static IEnumerable<object[]> Data => new List<object[]>
+    //{
+    //  new object[]
+    //  {
+    //    "UserName1", "Password1", "Password1", "abc1@email.com"
+    //  },
+    //  new object[]
+    //  {
+    //    "UserName2", "Password2", "Password2", "abc2@email.com"
+    //  }
+    //};
   }
 }
