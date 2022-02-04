43dd7a155914ae5e61809a2bf6fd87b38d6b20cc
Howard Choi <hchoi84@gmail.com>
2022-02-04 10:21:22 -0800

    3-10 IWebDriver and IClassFixture

---
+++ SeleniumXUnitBasic/DriverFixture.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumXUnitBasic
+{
+  public class DriverFixture : IDisposable
+  {
+    private readonly IWebDriver _driver;
+
+    public IWebDriver WebDriver { get { return _driver; } }
+
+    public DriverFixture()
+    {
+      new DriverManager().SetUpDriver(new ChromeConfig());
+      _driver = new ChromeDriver();
+    }
+
+    public void Dispose()
+    {
+      _driver.Quit();
+    }
+  }
+}
--- SeleniumXUnitBasic/UnitTest1.cs
+++ SeleniumXUnitBasic/UnitTest1.cs
@@ -8,17 +8,19 @@ using Xunit;
 
 namespace SeleniumXUnitBasic
 {
-  public class UnitTest1
+  public class UnitTest1 : IClassFixture<DriverFixture>
   {
-    private IWebDriver _driver;
+    private readonly IWebDriver _driver;
+
+    public UnitTest1(DriverFixture driverFixture)
+    {
+      _driver = driverFixture.WebDriver;
+      _driver.Navigate().GoToUrl(new Uri("https://localhost:7207/"));
+    }
 
     [Fact]
     public void Test1()
     {
-      new DriverManager().SetUpDriver(new ChromeConfig());
-      _driver = new ChromeDriver();
-
-      _driver.Navigate().GoToUrl(new Uri("https://localhost:7207/"));
       _driver.FindElement(By.LinkText("Product")).Click();
       _driver.FindElement(By.LinkText("Create")).Click();
 
@@ -29,7 +31,36 @@ namespace SeleniumXUnitBasic
       SelectElement? select = new(_driver.FindElement(By.Id("Product_ProductType")));
       select.SelectByValue("2");
       _driver.FindElement(By.Id("Create")).Submit();
-      _driver.Quit();
+    }
+
+    [Fact]
+    public void Test2()
+    {
+      _driver.FindElement(By.LinkText("Product")).Click();
+      _driver.FindElement(By.LinkText("Create")).Click();
+
+      _driver.FindElement(By.Id("Product_Name")).SendKeys("Desk");
+      _driver.FindElement(By.Id("Product_Description")).SendKeys("Standing Desk");
+      _driver.FindElement(By.Id("Product_Price")).Clear();
+      _driver.FindElement(By.Id("Product_Price")).SendKeys("100");
+      SelectElement? select = new(_driver.FindElement(By.Id("Product_ProductType")));
+      select.SelectByValue("2");
+      _driver.FindElement(By.Id("Create")).Submit();
+    }
+
+    [Fact]
+    public void Test3()
+    {
+      _driver.FindElement(By.LinkText("Product")).Click();
+      _driver.FindElement(By.LinkText("Create")).Click();
+
+      _driver.FindElement(By.Id("Product_Name")).SendKeys("Chair");
+      _driver.FindElement(By.Id("Product_Description")).SendKeys("Sitting Chair");
+      _driver.FindElement(By.Id("Product_Price")).Clear();
+      _driver.FindElement(By.Id("Product_Price")).SendKeys("100");
+      SelectElement? select = new(_driver.FindElement(By.Id("Product_ProductType")));
+      select.SelectByValue("3");
+      _driver.FindElement(By.Id("Create")).Submit();
     }
   }
 }
