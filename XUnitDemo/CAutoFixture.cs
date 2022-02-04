79490782e503cdf3ff182687d2f18d99f1758246
Howard Choi <hchoi84@gmail.com>
2022-02-03 18:02:13 -0800

    Section14-137 AutoFixture

--- XUnitDemo/CAutoFixture.cs
+++ XUnitDemo/CAutoFixture.cs
@@ -18,7 +18,7 @@ namespace XUnitDemo
     }
 
     [Fact]
-    public void ClassFixtureTestMemberData()
+    public void AutoFixtureTest()
     {
       var driver = _webDriverFixture.ChromeDriver;
       driver.Navigate().GoToUrl("http://eaapp.somee.com");
@@ -34,6 +34,26 @@ namespace XUnitDemo
       driver.FindElement(By.Id("Password")).SendKeys(pwd);
       driver.FindElement(By.Id("ConfirmPassword")).SendKeys(cpwd);
       driver.FindElement(By.Id("Email")).SendKeys(mailAddress.Address);
+
+      _testOutputHelper.WriteLine("Test Completed");
+    }
+
+    [Fact]
+    public void AutoFixtureTestWithClass()
+    {
+      var driver = _webDriverFixture.ChromeDriver;
+      driver.Navigate().GoToUrl("http://eaapp.somee.com");
+
+      //using AutoFixture, we can create a random value for specified type.
+      //var user = new Fixture().Create<RegisterUserModel>();
+      //var user = new Fixture().Build<RegisterUserModel>().Without(x => x.Email).Create();
+      var user = new Fixture().Build<RegisterUserModel>().With(x => x.Email, "abc@ea.com").Create();
+
+      driver.FindElement(By.LinkText("Register")).Click();
+      driver.FindElement(By.Id("UserName")).SendKeys(user.Name);
+      driver.FindElement(By.Id("Password")).SendKeys(user.Password);
+      driver.FindElement(By.Id("ConfirmPassword")).SendKeys(user.CPassword);
+      driver.FindElement(By.Id("Email")).SendKeys(user.Email);
 
       _testOutputHelper.WriteLine("Test Completed");
     }
---
+++ XUnitDemo/RegisterUserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace XUnitDemo
+{
+  public class RegisterUserModel
+  {
+    public string Name { get; set; }
+    public string Password { get; set; }
+    public string CPassword { get; set; }
+    public string Email { get; set; }
+  }
+}
