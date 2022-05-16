28810301712c35f1ec56f9acbf33061a6e6cdb2b
Howard Choi <hchoi84@gmail.com>
2022-05-16 14:41:15 -0700

    6-61 Add DB Connection

--- EATestBDD/Startup.cs
+++ EATestBDD/Startup.cs
@@ -3,6 +3,9 @@ using EATestFramework.Extensions;
 using EATestBDD.Pages;
 using Microsoft.Extensions.DependencyInjection;
 using SolidToken.SpecFlow.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using ProductAPI.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace EATestBDD;
 
@@ -13,6 +16,15 @@ public static class Startup
   {
     ServiceCollection services = new();
     services.UserWebDriverInitializer();
+
+    string projectPath = AppDomain.CurrentDomain.BaseDirectory.Split(new String[] { @"bin\" }, StringSplitOptions.None)[0];
+    IConfigurationRoot configuration = new ConfigurationBuilder()
+      .SetBasePath(projectPath)
+      .AddJsonFile("appsettings.json")
+      .Build();
+    string connectionString = configuration.GetConnectionString("DefaultConnection");
+    services.AddDbContext<ProductDbContext>(option => option.UseSqlServer(connectionString));
+
     services.AddScoped<IBrowserDriver, BrowserDriver>();
     services.AddScoped<IDriverFixture, DriverFixture>();
     services.AddScoped<IHomePage, HomePage>();
